1957fbe4629c9140e898669baad36c4bdc766fef
anseljohn <jjohnanselmo@gmail.com>
2021-11-22 15:20:44 -0600

    Added structure for turning off startup

--- cmd.rb
+++ cmd.rb
@@ -29,6 +29,28 @@ class Cmd
     end
   end
 
+  class Config
+    def self.call(options=["config"])
+      options.shift()
+
+      case options[0]
+      when "startup"
+        on_off = options[1] == '-on' ? true : false
+        case options[1]
+        when '-off' || '-on'
+          config_file = Glob::wdir['config']
+          config_data = Glob::FileHandler.read(config_file)
+          config_data['start_msg'] = on_off
+          Glob::FileHandler.write(config_file, config_data)
+        else
+          puts "Invalid options for startup config modification"
+        end
+      else
+        puts "Invalid config modifications"
+      end
+    end
+  end
+  
   # Log in, create a new user, configure users, etc.
   class Stud
     def self.call(options=["stud"])
--- glob.rb
+++ glob.rb
@@ -64,9 +64,13 @@ module Glob
     {
       "help".colorize(:light_white) => "\tGet information about TAssign commands",
       "quit".colorize(:light_white) => "\tQuit the program",
+      "config".colorize(:light_white) => "\tConfigure TAssign options.\n"\
+                      "\tOptions:\n".colorize(:green)+""\
+                      "\t\tstartup -on[off]".colorize(:light_white) + " : " + "config startup -off".colorize(:light_white)+"\n"\
+                      "\t\t\tTurn off the startup tip for getting started.".colorize(:green)+"",
       "clear".colorize(:light_white) => "\tClear the terminal",
       "stud".colorize(:light_white) => "\tSelect or create a student\n".colorize(:green)+""\
-      "\tOptions:\n".colorize(:green)+""\
+                      "\tOptions:\n".colorize(:green)+""\
                       "\t\t-n".colorize(:light_white) + " : " + "stud -n <username>\n".colorize(:light_white)+""\
                       "\t\t\tCreates a new student with the username username\n".colorize(:green)+""\
                       "\t\t-a[v]".colorize(:light_white) + " : " + "stud -a[v]\n".colorize(:light_white)+""\
