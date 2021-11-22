275975e6f82c78eb8e50f8c9d615b889db295988
anseljohn <jjohnanselmo@gmail.com>
2021-11-21 23:27:37 -0600

    Tassign command permission denied

--- install.sh
+++ install.sh
@@ -1,49 +1,33 @@
-#!/bin/bash
+#/bin/bash
 
 t_src=/usr/local/bin/tassign/src
 
-mkdir -p $t_src
-mv * $t_src
+if ! test -d $t_src; then
+    mkdir -p $t_src
+    cp -r * $t_src
 
-alias tassign='ruby /usr/local/bin/tassign/src/tassign.rb'
+    alias tassign='ruby /usr/local/bin/tassign/src/tassign.rb'
 
-zsh=~/.zshrc
-bash=~/.bashrc
+    zsh=~/.zshrc
+    bash=~/.bashrc
 
-env=$zsh
-
-case $SHELL in
-*/zsh)
     env=$zsh
-    ;;
-*/bash)
-    env=$bash
-*)
-    echo "$SHELL not supported"
-    return
-esac
 
-touch $env
-echo "source $t_src/call.sh" >> $env
-chmod 775 $t_src/call.sh
+    case $SHELL in
+    */zsh)
+        env=$zsh
+        ;;
+    */bash)
+        env=$bash
+        ;;
+    *)
+        echo "$SHELL not supported"
+        return
+    esac
 
-#case $SHELL in
-#*/zsh)
-#    if ! test -e $zsh; then
-#        touch ~/.zshrc
-#    fi
-#    echo "source ~$t_src/call.sh" >> $zsh
-#    ;;
-#*/bash)
-#    if ! test -e $bash; then
-#        touch ~/.bashrc
-#    fi
-#    echo "source ~$t_src/call.sh" >> $bash
-#    ;;
-#*)
-#    echo "$SHELL not supported."
-#esac
-#
-#
-#
-rm -rf ../TAssign
+    touch $env
+    echo "source $t_src/call.sh" >> $env
+    chmod 775 $t_src/call.sh
+else
+    echo "TAssign already installed."
+fi
