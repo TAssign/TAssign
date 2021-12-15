50f94a72f14b91d523b8ea0543efeab751be26aa
anseljohn <jjohnanselmo@gmail.com>
2021-12-15 00:03:42 -0600

    config startup message print - start only print when changing

--- lib/cmd/config.rb
+++ lib/cmd/config.rb
@@ -9,21 +9,25 @@ class Config
 
     case options[0]
     when "startup"
+      msg = "Startup message turned "
       case options[1]
       when 'off'
         config_file = Glob::wdir['config']
         config_data = Glob::FileHandler.read(config_file)
         config_data['start_msg'] = false
         Glob::FileHandler.write(config_file, config_data)
+        msg += "off."
       when 'on'
         config_file = Glob::wdir['config']
         config_data = Glob::FileHandler.read(config_file)
         config_data['start_msg'] = true
         Glob::FileHandler.write(config_file, config_data)
+        msg += "on."
       else
         puts "Invalid options for startup config modification"
         puts "Use ".colorize(:green) + " config startup -on/off".colorize(:light_whie)
       end
+      puts(msg)
     else
       puts "Invalid config modifications"
     end
