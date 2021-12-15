00d685f45ab338fa3331bbf51dd0851f6179ee02
anseljohn <jjohnanselmo@gmail.com>
2021-12-15 11:02:00 -0600

    Start turning cmd docs to glob colorize

--- glob.rb
+++ glob.rb
@@ -74,14 +74,14 @@ module Glob
   # Get command documentation
   def cmds
     {
-      "help".colorize(:light_white) => "\tGet information about TAssign commands",
-      "quit".colorize(:light_white) => "\tQuit the program",
-      "config".colorize(:light_white) => "\tConfigure TAssign options.\n"\
-                      "\tOptions:\n".colorize(:green)+""\
-                      "\t\tstartup on[off]".colorize(:light_white) + " : " + "config startup off".colorize(:light_white)+"\n"\
-                      "\t\t\tTurn off the startup tip for getting started.".colorize(:green)+"",
-      "clear".colorize(:light_white) => "\tClear the terminal",
-      "stud".colorize(:light_white) => "\tSelect or create a student\n".colorize(:green)+""\
+      Glob.white("help") => "\tGet information about TAssign commands",
+      Glob.white("quit") => "\tQuit the program",
+      Glob.white("config") => "\tConfigure TAssign options.\n"\
+                              ""+Glob.green("\tOptions:\n")+""\
+                              ""+Glob.white("\t\tstartup on[off]") + " : " + Glob.white("config startup off")+"\n"\
+                              ""+Glob.green("\t\t\tTurn off the startup tip for getting started.")+"",
+      Glob.white("clear") => "\tClear the terminal",
+      Glob.white("stud") => "\tSelect or create a student\n".colorize(:green)+""\
                       "\tOptions:\n".colorize(:green)+""\
                       "\t\t-n".colorize(:light_white) + " : " + "stud -n <username>\n".colorize(:light_white)+""\
                       "\t\t\tCreates a new student with the username username\n".colorize(:green)+""\
