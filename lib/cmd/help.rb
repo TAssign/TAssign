1b1024b11e4dd295e6e96ebc5eec2618b24cfe71
anseljohn <jjohnanselmo@gmail.com>
2021-12-16 13:14:27 -0600

    Help for specific commands

--- lib/cmd/help.rb
+++ lib/cmd/help.rb
@@ -6,16 +6,30 @@ end
 
 class Help
   def self.call(opts)
-    put_first = [Help, Clear, Quit]
-    put_first.each { |cmd|
-      doc cmd
-    }
-    all_cmds.each do |cmd|
-      if not put_first.include? cmd
+    opts.shift()
+
+    if opts.length == 0
+      put_first = [Help, Clear, Quit]
+      put_first.each { |cmd|
         doc cmd
+      }
+      all_cmds(false).each do |cmd|
+        if not put_first.include? cmd
+          doc cmd
+        end
       end
+      puts ""
+    elsif opts.length > 1
+      puts Glob.white("help") + " only takes 0 or 1 arguments."
+    elsif all_cmds(true).include? opts[0].capitalize or opts[0] == "exit"
+      opt = opts[0]
+      if opt == "exit"
+        opt = "quit"
+      end
+      doc(classify(opt))
+    else
+      puts "Command " + Glob.white(opts[0]) + " not found..."
     end
-    puts ""
   end
 
   def self.name
@@ -27,6 +41,9 @@ class Help
   end
 
   private
+  def self.classify(class_str)
+    Object.const_get(class_str.capitalize)
+  end
   def self.doc(cmd)
     puts ""
     puts cmd.name + "\n\t" + cmd.desc
@@ -35,12 +52,15 @@ class Help
     end
   end
 
-  def self.all_cmds
+  def self.all_cmds(str)
     all = []
 
     Dir.foreach("lib/cmd/") do |file|
       if file != "." and file != ".."
-        cmd = Object.const_get(File.basename(file, ".rb").capitalize)
+        cmd = File.basename(file, ".rb").capitalize
+        if not str
+          cmd = classify(cmd)
+        end
         all.push(cmd)
       end
     end
