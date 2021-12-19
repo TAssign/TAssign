c4ff151bd1e6051f15690ab59c3df193365220b0
anseljohn <jjohnanselmo@gmail.com>
2021-12-18 20:54:24 -0600

    Start todo list

---
+++ lib/cmd/logged/td.rb
@@ -0,0 +1,28 @@
+require_relative '../../../glob.rb'
+
+include Glob
+
+class Td
+
+  def self.call(opts)
+  end
+
+  def self.name
+    "td"
+  end
+
+  def self.desc
+    "Select or create a to-do list"
+  end
+
+  def self.options
+    "\tOptions:\n"\
+    ""+Glob.white("\t\t-n") + " : " + Glob.white("td -n name")+"\n"\
+    "\t\t\tCreate a new to-do list with name " + Glob.white("name\n")+""\
+    ""+Glob.white("\t\t-d") + " : " + Glob.white("td -d name")+"\n"\
+    "\t\t\tDelete a to-do list with name " + Glob.white("name")
+  end
+
+end
+
+puts Td.options
